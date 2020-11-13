212c264e7069c0b7df863cdb947467962a0dd036
elkinjimenez <elkin.jiga@hotmail.com>
2020-11-13 10:41:37 -0500

    Inicio de proyecto

---
+++ src/main/java/com/api/almacanela/AlmacanelaApplication.java
@@ -0,0 +1,13 @@
+package com.api.almacanela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacanelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacanelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/almacanela/AlmacanelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.almacanela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacanelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
